c0cba7a57e9492ca5a73ed3b1f29fdbb40ab945f
Iaroslav Mokroguz <maustrauk@gmail.com>
2021-06-16 23:04:46 -0400

    Instaling auth0

--- src/components/OnStart/Title.jsx
+++ src/components/OnStart/Title.jsx
@@ -61,7 +61,7 @@ const Title = () => {
       </defs>
       <text {...textStyle}>
         <textPath xlinkHref="#ContextPath">
-          Context
+          React
         </textPath>
       </text>
       <text {...textStyle}>
