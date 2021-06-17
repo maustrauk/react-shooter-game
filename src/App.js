a4f3acc6b3a81e0267e977b1a322baa3b5562e4b
Iaroslav Mokroguz <maustrauk@gmail.com>
2021-06-16 20:05:11 -0400

    Adding redux to the project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class App extends Component  {
-  
-  componentDidMount() {
-
-  }
 
   render() {
     return (
-      <div>
-        
-      </div>);
+      <div className="App">
+        <h1>{this.props.message}</h1>
+      </div>
+    );
   }
 
-}
+};
+
+App.propTypes = {
+  message: PropTypes.string.isRequired,
+};
 
 export default App;
