8ab437e64d277c3a25fac609e0afe61f461cdf4c
Iaroslav Mokroguz <maustrauk@gmail.com>
2021-06-21 07:52:17 -0400

    Adding number of balls to gamePlay component

--- src/components/GamePlay/GamePlay.jsx
+++ src/components/GamePlay/GamePlay.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 import CurrentScore from './CurrentScore';
 import Heart from './Heart';
+import CannonBall from '../CannonBalls/CannonBall';
 
 const GamePlay = (props) => {
 
@@ -17,16 +18,29 @@ const GamePlay = (props) => {
         lives.push(heartPosition);
     }
 
+    const balls = [];
+
+    for (let i = 0; i < props.balls; i++) {
+        const ballPosition = {
+            x: 140 + ( i * 70 ),
+            y: 45
+        }
+
+        balls.push(ballPosition);
+    }
+
 
     return <g>
         <CurrentScore score={props.score} />
         {lives.map((live, id) => (<Heart key={`Heart${id}`} position={live} />))}
+        {balls.map((ball, id) => (<CannonBall key={`Ball${id}`} position={ball} animationEnd={{x: 0, y: 0}}/>))}
     </g>
 };
 
 GamePlay.propTypes = {
     lives: PropTypes.number.isRequired,
     score: PropTypes.number.isRequired,
+    balls: PropTypes.number.isRequired,
   };
 
 export default GamePlay;
