846e5f438173b7c04ac01e1adb407827633566b3
Iaroslav Mokroguz <maustrauk@gmail.com>
2021-06-21 07:23:45 -0400

    Removing leader board

--- src/components/OnStart/OnStart.jsx
+++ src/components/OnStart/OnStart.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { signIn } from 'auth0-web';
 
 import Title from './Title';
 import StartGame from './StartGame';
-import Leaderboard from './Leaderboard';
 
 const OnStart = (props) => {
 
     return <g>
         <Title/>
         <StartGame onClick={() => props.startGame()}/>
-        <Leaderboard currentPlayer={props.currentPlayer} authenticate={signIn} leaderboard={props.players}/>
     </g>
 };
 
