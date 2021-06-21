269da750de48dd0c2f2e1cb1e7cecfb15b8a0f33
Iaroslav Mokroguz <maustrauk@gmail.com>
2021-06-21 07:34:32 -0400

    Removing mockdata

--- src/components/OnStart/OnStart.jsx
+++ src/components/OnStart/OnStart.jsx
@@ -4,8 +4,6 @@ import PropTypes from 'prop-types';
 import Title from './Title';
 import StartGame from './StartGame';
 
-import { leaderboard } from '../../utils/mockData'; 
-
 const OnStart = (props) => {
 
     return <g>
